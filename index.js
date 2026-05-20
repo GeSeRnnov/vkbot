006269ba2a073e7280020f5f8c47a4fe3332e6d3
Georg Rukomin <villian_2007@mail.ru>
2026-05-20 08:34:48 +0300

    downgrade vk-io

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+require('dotenv').config(); 
 const { VK } = require('vk-io');
 const wol = require('wake_on_lan');
+
+const vkToken = process.env.TOKEN
+// Вставьте сюда ваш числовой ID ВКонтакте
+const ADMIN_ID = process.env.USER_ID; 
+const computerMac = process.env.MAC.replace(/-/g, ':').toLowerCase();; // Сюда вставим MAC позже
+
+if (!(vkToken && ADMIN_ID && computerMac)) {
+    console.error('❌ Ошибка: Не все переменные окружения заданы в файле .env');
+    process.exit(1);
+}
 
 const vk = new VK({
     token: process.env.TOKEN
 });
 
-// Вставьте сюда ваш числовой ID ВКонтакте
-const ADMIN_ID = process.env.USER_ID; 
-const computerMac = process.env.MAC; // Сюда вставим MAC позже
-
-vk.updates.on('message_new', async (context) => {
+// vk.updates.on('message_new', async (context) => {
+vk.updates.on('message', async (context) => {
     if (context.senderId !== ADMIN_ID) return; 
     const text = context.text ? context.text.toLowerCase().trim() : '';
 
